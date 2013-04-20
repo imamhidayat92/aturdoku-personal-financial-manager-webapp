4b3bc20603dad787fc7742f857005e31f0cc2ff6
Imam Hidayat <imam.hidayat92@gmail.com>
2013-04-20 18:06:17 +0700

    Minor changes in AssetsController

--- app/Controller/AssetsController.php
+++ app/Controller/AssetsController.php
@@ -10,7 +10,18 @@ class AssetsController extends AppController {
     }
     
     public function edit($asset_id) {
+        $this->set('title_for_layout', "Edit Data Aset");
         
+        if ($this->request->isPost()) {
+            $this->request->data['Transaction']['id'] = $asset_id;
+            
+            if ($this->Transaction->save($this->request->data)) {
+                $this->Session->setFlash("", "flash_custom");
+            }
+        }
+        
+        $asset = $this->Asset->findByid($asset_id);
+        $this->set('asset', $asset);
     }
     
     public function delete($asset_id) {
